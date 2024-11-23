ace2d8d0c8db7a5ffe8144a2837e77f2b4ab5154
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-23 04:24:01 -0300

    criação da classe slug e teste

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,15 +1,18 @@
 import { randomUUID } from 'node:crypto'
+import { Slug } from './value-objects/slug'
 
 interface QuestionProps {
   title: string
   description: string
   authorId: string
+  slug: Slug
 }
 
 export class Question {
   public id?: string
   public title: string
   public description: string
+  public slug: Slug
   public authorId: string
 
   constructor(props: QuestionProps, id?: string) {
@@ -17,5 +20,6 @@ export class Question {
     this.title = props.title
     this.description = props.description
     this.authorId = props.authorId
+    this.slug = props.slug
   }
 }
---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,7 @@
+import { expect, it } from 'vitest'
+import { Slug } from './slug'
+
+it('should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('An example title')
+  expect(slug.value).toEqual('an-example-title')
+})
