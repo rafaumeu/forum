cfaf639bfaeaf6b651aee6e949cd59b7267e2e13
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-24 17:01:34 -0300

    refatorando pastas

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,6 +1,6 @@
-import { Answer } from '@/domain/entities/answer'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
-import { AnswersRepository } from '@/domain/repositories/answers-repository'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
 const fakeAnswersRepository: AnswersRepository = {
