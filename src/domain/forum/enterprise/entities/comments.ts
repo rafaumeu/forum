9f2b38f3658d0f56ca51f54fae07042181dd8e10
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-02 10:18:46 -0300

    criando mappers do prisma

--- src/domain/forum/enterprise/entities/comments.ts
+++ src/domain/forum/enterprise/entities/comments.ts
@@ -5,13 +5,23 @@ export interface CommentProps {
   authorId: UniqueEntityId
   content: string
   createdAt: Date
-  updatedAt?: Date
+  updatedAt?: Date | null
 }
+
 export abstract class Comment<
   Props extends CommentProps,
 > extends Entity<Props> {
   get authorId() {
     return this.props.authorId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
   }
 
   get createdAt() {
@@ -22,20 +32,7 @@ export abstract class Comment<
     return this.props.updatedAt
   }
 
-  get excerpt() {
-    return this.content.substring(0, 120).trimEnd().concat('...')
-  }
-
   private touch() {
     this.props.updatedAt = new Date()
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
   }
 }
