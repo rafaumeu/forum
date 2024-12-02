2dce34d9bb47826c7b9e41772af770389065cba4
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-02 14:13:55 -0300

    refatorando testes E2E

--- src/infra/http/controllers/fetch-recent-questions.controller.e2e.spec.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.e2e.spec.ts
@@ -1,62 +1,44 @@
 import { AppModule } from '@/infra/app.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { DatabaseModule } from '@/infra/database/database.module'
 
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
 
 describe('Fetch recent questions (E2E)', () => {
   let app: INestApplication
-  let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
     }).compile()
     app = moduleRef.createNestApplication()
-    prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
     jwt = moduleRef.get(JwtService)
     await app.init()
   })
   test('[GET] /questions', async () => {
-    const user = await prisma.user.create({
-      data: {
-        name: 'John Doe',
-        email: 'fWl8o@example.com',
-        password: '123456',
-      },
-    })
-    const accessToken = jwt.sign({ sub: user.id })
-    await prisma.question.createMany({
-      data: [
-        {
-          authorId: user.id,
-          title: 'New question',
-          slug: 'new-question',
-          content: 'New content',
-        },
-        {
-          authorId: user.id,
-          title: 'New question 2',
-          slug: 'new-question-2',
-          content: 'New content 2',
-        },
-        {
-          authorId: user.id,
-          title: 'New question 3',
-          slug: 'new-question-3',
-          content: 'New content 3',
-        },
-        {
-          authorId: user.id,
-          title: 'New question 4',
-          slug: 'new-question-4',
-          content: 'New content 4',
-        },
-      ],
-    })
+    const user = await studentFactory.makePrismaStudent()
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+    await Promise.all([
+      questionFactory.makePrismaQuestion({
+        authorId: user.id,
+        title: 'New question',
+      }),
+      questionFactory.makePrismaQuestion({
+        authorId: user.id,
+        title: 'New question 2',
+      }),
+    ])
+
     const response = await request(app.getHttpServer())
       .get('/questions')
       .set('Authorization', `Bearer ${accessToken}`)
@@ -64,20 +46,16 @@ describe('Fetch recent questions (E2E)', () => {
     expect(response.statusCode).toBe(200)
 
     expect(response.body).toEqual({
-      questions: [
+      questions: expect.arrayContaining([
         expect.objectContaining({
           title: 'New question',
+          slug: 'new-question',
         }),
         expect.objectContaining({
           title: 'New question 2',
+          slug: 'new-question-2',
         }),
-        expect.objectContaining({
-          title: 'New question 3',
-        }),
-        expect.objectContaining({
-          title: 'New question 4',
-        }),
-      ],
+      ]),
     })
   })
 })
--- test/factories/make-answer.ts
+++ test/factories/make-answer.ts
@@ -1,6 +1,9 @@
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Answer, AnswerProps } from '@/domain/forum/enterprise/entities/answer'
+import { PrismaAnswerMapper } from '@/infra/database/mappers/prisma-answer-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeAnswer(
   override: Partial<AnswerProps> = {},
@@ -16,4 +19,17 @@ export function makeAnswer(
     id,
   )
   return answer
+}
+
+@Injectable()
+export class AnswerFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswer(data: Partial<AnswerProps> = {}): Promise<Answer> {
+    const answer = makeAnswer(data)
+    await this.prisma.answer.create({
+      data: PrismaAnswerMapper.toPrisma(answer),
+    })
+    return answer
+  }
 }
