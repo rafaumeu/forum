8673c85b42882d36f2deb9b77a2bbc54313e7dbe
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-06 08:01:57 -0300

    Implementando cache com Redis

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,9 +1,18 @@
+import { RedisCacheRepository } from '@/infra/cache/redis/redis-cache-repository'
+import { RedisService } from '@/infra/cache/redis/redis.service'
 import { EnvModule } from '@/infra/env/env.module'
-import { EnvService } from '@/infra/env/env.service'
 import { Module } from '@nestjs/common'
+import { CacheRepository } from './cache-repository'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    RedisService,
+    {
+      provide: CacheRepository,
+      useClass: RedisCacheRepository,
+    },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
