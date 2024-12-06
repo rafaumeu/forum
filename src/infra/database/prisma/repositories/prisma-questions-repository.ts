78dec42904559c4cea2487171185335c0e79360d
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-06 07:07:36 -0300

    integrando cache no prisma

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -4,6 +4,7 @@ import { QuestionAttachmentsRepository } from '@/domain/forum/application/reposi
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details'
+import { CacheRepository } from '@/infra/cache/cache-repository'
 import { PrismaQuestionDetailsMapper } from '@/infra/database/mappers/prisma-question-details-mapper'
 import { PrismaQuestionMapper } from '@/infra/database/mappers/prisma-question-mapper'
 import { PrismaService } from '@/infra/database/prisma/prisma.service'
@@ -13,6 +14,7 @@ import { Injectable } from '@nestjs/common'
 export class PrismaQuestionsRepository implements QuestionsRepository {
   constructor(
     private prisma: PrismaService,
+    private cache: CacheRepository,
     private questionAttachmentsRepository: QuestionAttachmentsRepository,
   ) {}
 
@@ -46,6 +48,11 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   }
 
   async findDetailsBySlug(slug: string): Promise<QuestionDetails | null> {
+    const cacheHit = await this.cache.get(`question:${slug}:details`)
+    if (cacheHit) {
+      const cachedData = JSON.parse(cacheHit)
+      return cachedData
+    }
     const question = await this.prisma.question.findUnique({
       where: { slug },
       include: { attachments: true, author: true },
@@ -53,7 +60,9 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     if (!question) {
       return null
     }
-    return PrismaQuestionDetailsMapper.toDomain(question)
+    const questionDetails = PrismaQuestionDetailsMapper.toDomain(question)
+    this.cache.set(`question:${slug}:details`, JSON.stringify(questionDetails))
+    return questionDetails
   }
 
   async delete(question: Question): Promise<void> {
@@ -77,6 +86,7 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
       this.questionAttachmentsRepository.deleteMany(
         question.attachments.getRemovedItems(),
       ),
+      this.cache.delete(`question:${question.slug}:details`),
     ])
     DomainEvents.dispatchEventsForAggregate(question.id)
   }
